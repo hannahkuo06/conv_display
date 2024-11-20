173ab8ad4bc291cd2dd98c6c718344ba1bd86ab0
Hannah Kuo <hannah.kuo@servicenow.com>
2024-11-19 18:03:07 -0800

    display reward simultaneously

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import ast
+
 import streamlit as st
 import pandas as pd
 
@@ -17,11 +18,20 @@ if uploaded_file is not None:
     # Allow the user to select a row (index) from the DataFrame
     selected_row = st.number_input('Select a row', min_value=0, max_value=len(df)-1, step=1)
 
-    # Display the selected cell content
+    reward, selected = st.columns([1, 2])
+
     if selected_column and selected_row is not None:
-        cell_value = df.loc[selected_row, selected_column]
-        try:
-            convo = ast.literal_eval(df[selected_column][selected_row])
-            st.write(convo)
-        except Exception as e:
-            st.write(f"Cannot display this record: {e}")
+        with selected:
+            st.write(f"**{selected_column}**")
+            cell_value = df.loc[selected_row, selected_column]
+
+            if selected_column == "traj" or selected_column == "info":
+                try:
+                    convo = ast.literal_eval(cell_value)
+                    st.write(convo)
+                except Exception as e:
+                    st.write(f"Cannot display this record: {e}")
+
+        with reward:
+            st.write("**reward**")
+            st.write(df["reward"][selected_row])
